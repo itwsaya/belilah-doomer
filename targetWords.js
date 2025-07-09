49823fd8b164cfa8d263fc3fe5a241601f0ef371
itwsaya <itwsaya@googlegroups.com>
2025-07-08 21:12:11 -0700

    changed the bd for tomorrow

--- targetWords.js
+++ targetWords.js
@@ -2,6 +2,7 @@ const targetWords = [
   "everything is better with you!",
   "we hate the lausd bitch",
   "A person's mood can also be influenced by LSD.",
+  "let us see the probability you seeing me on tv",
   "belilah doomer",
   "i love you",
   "you make me so happy",
