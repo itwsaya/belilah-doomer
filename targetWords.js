ea3f84640d86a3a0a85f41cbc75a182af2191762
itwsaya <itwsaya@googlegroups.com>
2025-08-21 20:00:19 -0700

    jfaksoqd

--- targetWords.js
+++ targetWords.js
@@ -45,7 +45,6 @@ const targetWords = [
   "absolutely positively gorgeous",
   "what a yapper",
   "spanking, hair pulling, pinning you down roughly",
-  "",
   "delilah rose, how does your garden grow?"
 ];
 
